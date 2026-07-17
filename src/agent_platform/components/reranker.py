7e48cf8b8f3097756103e8a5e0b598b7166aaef9
Mathieu-Le-Gouill <mathieulegouill@outlook.com>
2026-07-17 13:30:38 +0200

    feat(components): reranker invocation with enforced batch_size

--- src/agent_platform/components/reranker.py
+++ src/agent_platform/components/reranker.py
@@ -6,14 +6,19 @@ from agent_platform.core.credentials import BaseCredentials
 from agent_platform.core.interfaces.reranking.base import BaseReranker
 from agent_platform.core.interfaces.reranking.config import RerankerConfig
 from agent_platform.components.base import Component
+from agent_platform.utils.batching import chunked
 
 CredentialsT = TypeVar("CredentialsT", bound=BaseCredentials)
 RerankerConfigT = TypeVar("RerankerConfigT", bound=RerankerConfig)
 
 T = TypeVar("T")
 
+RerankerInput = tuple[str, list[T]]
 
-class Reranker(Component[list[T], list[T]], Generic[CredentialsT, T, RerankerConfigT]):
+
+class Reranker(
+    Component[RerankerInput[T], list[T]], Generic[CredentialsT, T, RerankerConfigT]
+):
     def __init__(
         self,
         backend: BaseReranker[CredentialsT, T, RerankerConfigT],
@@ -22,5 +27,13 @@ class Reranker(Component[list[T], list[T]], Generic[CredentialsT, T, RerankerCon
         self._backend = backend
         self._config = config
 
-    async def arun(self, input: list[T]) -> list[T]:
-        return self._backend.rerank(input, self._config)
+    async def arun(self, input: RerankerInput[T]) -> list[T]:
+        query, items = input
+        batch_size = (self._config or RerankerConfig()).batch_size
+
+        results: list[T] = []
+        for batch in chunked(items, batch_size):
+            reranked = await self._backend.rerank(query, list(batch), self._config)
+            results.extend(reranked)
+
+        return results
